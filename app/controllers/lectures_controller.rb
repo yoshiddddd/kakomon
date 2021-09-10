8fb38f88e4f44c5fc8230ff864971f17dbb211d5
EC2 Default User <ec2-user@ip-172-31-25-36.us-east-2.compute.internal>
2021-09-10 11:45:59 +0000

    過去問共有２

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,8 +1,7 @@
 class LecturesController < ApplicationController
 def index
     @lectures=Department.find_by(id: params[:department]).lectures
-    @teachers= @lectures.ids
-    
+   
 end 
 
 def show
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,5 @@
 class Lecture < ApplicationRecord
   has_many :lecture_departments
-  has_many :teacher_lectures
+  has_many :lecture_teachers
+  has_many :teachers, through: :lecture_teachers
 end
